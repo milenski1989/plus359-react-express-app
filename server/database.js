446d94ce9086ce5faccf13840114091ebad26cf2
murkov <milenski1989@gmail.com>
2022-09-29 23:14:34 +0300

    New env variables for deploy

--- server/database.js
+++ server/database.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv').config()
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    port: process.env.MYSQL_PORT,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 })
