9e8f3640242ea7f1c5073c51c09ea5594d838be3
milenski1989 <milenski1989@gmail.com>
2024-02-29 14:10:08 +0200

    style navbar for mobile

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-undef */
 import { Link, useNavigate, useLocation } from "react-router-dom"
 import './NavBar.css'
+import { useMediaQuery } from "@mui/material";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { useState } from "react";
 
 const icons = {
     logo: require('./assets/logo359 gallery-white1.png')
@@ -10,6 +13,10 @@ const Navbar = () => {
 
     let navigate = useNavigate();
     const {pathname} = useLocation();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
+    console.log(isSmallDevice)
 
     const handleLogout = () => {
         window.localStorage.clear()
@@ -18,27 +25,60 @@ const Navbar = () => {
     }
 
     return  <>
-        <nav className="navbar">
-            <div className="navbar-elements-container">
-                <Link to='/'><img className="logo" alt="logo" src={icons.logo}/></Link>
-                <p className="current-location">{pathname.slice(10).replace(/%20/g, ' ').replace(/([A-Z])/g, ' $1')}</p>
-                <div className="right-side">
+        {isSmallDevice ?
+            <nav className="mobile-navbar-container">
+                <Link to='/'><img className="mobile-logo" alt="logo" src={icons.logo}/></Link>   
+                <div className={isOpen ? "mobile-nav-links-container" : "hidden"}>
                     <Link to='/' 
-                        className="nav-link"
+                        className="mobile-nav-link"
                     >Home</Link>
                     <Link to='/upload'
-                        className="nav-link"
+                        className="mobile-nav-link"
                     >Upload</Link>
                     <Link to='/account'
-                        className="nav-link"
+                        className="mobile-nav-link"
                     >Account</Link>
                     <Link to='/login'
                         onClick={handleLogout}
-                        className="nav-link"
+                        className="mobile-nav-link"
                     >Log Out</Link>
                 </div>
-            </div>
-        </nav> 
+                <div className="block lg:hidden">
+                    <button
+                        onClick={() => setIsOpen(!isOpen)}
+                        className="navbar-button"
+                    >
+                        {isOpen ? (
+                            <XIcon className="h-6 w-6" aria-hidden="true" />
+                        ) : (
+                            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                        )}
+                    </button>
+                </div>
+            </nav> 
+            :
+            <nav className="navbar">
+                <div className="navbar-elements-container">
+                    <Link to='/'><img className="logo" alt="logo" src={icons.logo}/></Link>
+                    <p className="current-location">{pathname.slice(10).replace(/%20/g, ' ').replace(/([A-Z])/g, ' $1')}</p>
+                    <div className="right-side">
+                        <Link to='/' 
+                            className="nav-link"
+                        >Home</Link>
+                        <Link to='/upload'
+                            className="nav-link"
+                        >Upload</Link>
+                        <Link to='/account'
+                            className="nav-link"
+                        >Account</Link>
+                        <Link to='/login'
+                            onClick={handleLogout}
+                            className="nav-link"
+                        >Log Out</Link>
+                    </div>
+                </div>
+            </nav>
+        } 
     </>
 }
 
