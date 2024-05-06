0b1c284fb71f57920ce4611d193b4c5d9fee4d7c
milenski1989 <milenski1989@gmail.com>
2024-05-06 16:33:47 +0300

    change origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ dotenv.config()
     
     //app.use(express.static(path.join(__dirname, '/build')));
     
-    app.use(cors({ origin: "http://localhost:3000" }))
+    app.use(cors({ origin: "https://artworks-management-app-client.vercel.app" }))
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
     app.use(cookieParser())
